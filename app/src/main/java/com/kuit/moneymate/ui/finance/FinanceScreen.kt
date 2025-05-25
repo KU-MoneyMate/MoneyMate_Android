066b6abf756bb3627235e37d5187c622627634ab
casperjr <hyunjw0515@gmail.com>
2025-05-26 06:38:04 +0900

    [feat]: bottomnav에 속하는 기본 화면 생성

---
+++ app/src/main/java/com/kuit/moneymate/ui/finance/FinanceScreen.kt
@@ -0,0 +1,25 @@
+package com.kuit.moneymate.ui.finance
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kuit.moneymate.ui.theme.MoneyMateTheme
+
+@Composable
+fun FinanceScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Text(
+            text = "FinanceScreen",
+            style = MoneyMateTheme.typography.head_02_B_20,
+            color = MoneyMateTheme.colors.black
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+    }
+}
