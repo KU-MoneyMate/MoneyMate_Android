7dc463f9d89877caffa40b6dd85a2b82c530d114
casperjr <hyunjw0515@gmail.com>
2025-05-26 06:36:30 +0900

    [chore]: project 기본 테마 및 폰트 세팅

--- app/src/main/java/com/kuit/moneymate/ui/theme/Color.kt
+++ app/src/main/java/com/kuit/moneymate/ui/theme/Color.kt
@@ -1,11 +1,44 @@
 package com.kuit.moneymate.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val BackgroundWhite = Color(0xFFF5F5F5)
+val DarkGray = Color(0xFF333333)
+val LightGray = Color(0x99333333)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Neutral100 = Color(0xFFF7F7F9)
+val Neutral300 = Color(0xFFE5E5E7)
+val Neutral500 = Color(0xFFA4A4A6)
+
+val DeepBlue = Color(0xFF0E0857)
+
+@Immutable
+data class MoneyMateColors(
+    val white: Color,
+    val black: Color,
+    val backgroundWhite: Color,
+    val darkGray: Color,
+    val lightGray: Color,
+    val neutral100: Color,
+    val neutral300: Color,
+    val neutral500: Color,
+    val deepBlue: Color,
+)
+
+val defaultMoneyMateColors = MoneyMateColors(
+    white = White,
+    black = Black,
+    backgroundWhite = BackgroundWhite,
+    darkGray = DarkGray,
+    lightGray = LightGray,
+    neutral100 = Neutral100,
+    neutral300 = Neutral300,
+    neutral500 = Neutral500,
+    deepBlue = DeepBlue,
+)
+
+val LocalMoneyMateColorsProvider = staticCompositionLocalOf { defaultMoneyMateColors }
--- app/src/main/java/com/kuit/moneymate/ui/theme/Type.kt
+++ app/src/main/java/com/kuit/moneymate/ui/theme/Type.kt
@@ -1,34 +1,105 @@
 package com.kuit.moneymate.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.kuit.moneymate.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val moneyMateFontBold = FontFamily(Font(R.font.pretendard_bold))
+val moneyMateFontSemiBold = FontFamily(Font(R.font.pretendard_semibold))
+val moneyMateFontMedium = FontFamily(Font(R.font.pretendard_medium))
+val moneyMateFontRegular = FontFamily(Font(R.font.pretendard_regular))
+
+@Immutable
+data class MoneyMateTypography(
+    // Head
+    val head_01_B_24: TextStyle,
+    val head_02_B_20: TextStyle,
+    val head_03_B_16: TextStyle,
+    val head_03_SB_16: TextStyle,
+    val head_03_R_16: TextStyle,
+    val head_04_SB_14: TextStyle,
+    val head_05_B_10: TextStyle,
+
+    // Body
+    val body_01_M_14: TextStyle,
+    val body_01_R_14: TextStyle,
+    val body_02_SB_12: TextStyle,
+    val body_02_R_12: TextStyle,
+
+    // Caption
+    val caption_01_R_10: TextStyle
 )
+
+val defaultMoneyMateTypography = MoneyMateTypography(
+    // Head
+    head_01_B_24 = TextStyle(
+        fontFamily = moneyMateFontBold,
+        fontSize = 24.sp,
+        lineHeight = 24.sp
+    ),
+    head_02_B_20 = TextStyle(
+        fontFamily = moneyMateFontBold,
+        fontSize = 20.sp,
+        lineHeight = 20.sp
+    ),
+    head_03_B_16 = TextStyle(
+        fontFamily = moneyMateFontBold,
+        fontSize = 16.sp,
+        lineHeight = 16.sp
+    ),
+    head_03_SB_16 = TextStyle(
+        fontFamily = moneyMateFontSemiBold,
+        fontSize = 16.sp,
+        lineHeight = 16.sp
+    ),
+    head_03_R_16 = TextStyle(
+        fontFamily = moneyMateFontRegular,
+        fontSize = 16.sp,
+        lineHeight = 16.sp
+    ),
+    head_04_SB_14 = TextStyle(
+        fontFamily = moneyMateFontSemiBold,
+        fontSize = 14.sp,
+        lineHeight = 14.sp
+    ),
+    head_05_B_10 = TextStyle(
+        fontFamily = moneyMateFontBold,
+        fontSize = 10.sp,
+        lineHeight = 10.sp
+    ),
+
+    // Body
+    body_01_M_14 = TextStyle(
+        fontFamily = moneyMateFontMedium,
+        fontSize = 14.sp,
+        lineHeight = 14.sp
+    ),
+    body_01_R_14 = TextStyle(
+        fontFamily = moneyMateFontRegular,
+        fontSize = 14.sp,
+        lineHeight = 14.sp
+    ),
+    body_02_SB_12 = TextStyle(
+        fontFamily = moneyMateFontSemiBold,
+        fontSize = 12.sp,
+        lineHeight = 12.sp
+    ),
+    body_02_R_12 = TextStyle(
+        fontFamily = moneyMateFontRegular,
+        fontSize = 12.sp,
+        lineHeight = 12.sp
+    ),
+
+    // Caption
+    caption_01_R_10 = TextStyle(
+        fontFamily = moneyMateFontRegular,
+        fontSize = 10.sp,
+        lineHeight = 10.sp
+    )
+)
+
+val LocalMoneyMateTypographyProvider = staticCompositionLocalOf { defaultMoneyMateTypography }
