2c4ecc4eb7fd9aee5337cc6ab7bd18d25d42f620
casperjr <hyunjw0515@gmail.com>
2025-05-26 08:17:16 +0900

    [feat]: 사용할 repository 작성

---
+++ app/src/main/java/com/moneymate/moneymate/data/repository/ManageRepository.kt
@@ -0,0 +1,9 @@
+package com.moneymate.moneymate.data.repository
+
+import com.moneymate.moneymate.data.service.ManageService
+
+class ManageRepository(
+    private val manageService: ManageService
+) {
+
+}
