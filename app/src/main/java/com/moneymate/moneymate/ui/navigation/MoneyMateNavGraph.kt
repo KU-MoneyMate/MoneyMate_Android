d09d6a2bae6f2d678269fbfada3632ef54230c5e
casperjr <hyunjw0515@gmail.com>
2025-07-05 18:58:55 +0900

    [refactor]: 회원가입/로그인 관련 화면들 별도의 AuthNavGraph로 분리, viewmodel 공유 코드 수정

--- app/src/main/java/com/moneymate/moneymate/ui/navigation/MoneyMateNavGraph.kt
+++ app/src/main/java/com/moneymate/moneymate/ui/navigation/MoneyMateNavGraph.kt
@@ -6,22 +6,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.moneymate.moneymate.data.dto.account.response.AccountInfo
+import com.moneymate.moneymate.ui.asset.screen.AddAccountScreen
+import com.moneymate.moneymate.ui.asset.screen.AddAssetScreen
 import com.moneymate.moneymate.ui.asset.screen.HomeScreen
+import com.moneymate.moneymate.ui.asset.screen.TransactionHistoryScreen
 import com.moneymate.moneymate.ui.finance.screen.FinanceScreen
 import com.moneymate.moneymate.ui.manage.screen.ManageScreen
 import com.moneymate.moneymate.ui.mypage.screen.MyPageScreen
-import com.moneymate.moneymate.ui.asset.screen.AddAccountScreen
-import com.moneymate.moneymate.ui.asset.screen.AddAssetScreen
-import com.moneymate.moneymate.ui.asset.screen.TransactionHistoryScreen
-import com.moneymate.moneymate.ui.auth.screen.LoginScreen
-import com.moneymate.moneymate.ui.auth.screen.SignUpIDScreen
-import com.moneymate.moneymate.ui.auth.screen.SignUpNameScreen
-import com.moneymate.moneymate.ui.auth.screen.SignUpPWScreen
-import com.moneymate.moneymate.ui.auth.screen.SignUpPhoneScreen
-import com.moneymate.moneymate.ui.auth.screen.SignUpVerificationScreen
 import kotlinx.serialization.json.Json
-import com.moneymate.moneymate.ui.manage.screen.RetireInputScreen
-import com.moneymate.moneymate.ui.manage.screen.RetireResultScreen
 
 @Composable
 fun MoneyMateNavGraph(
@@ -30,63 +22,10 @@ fun MoneyMateNavGraph(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Route.Login.route
-    ){
+        startDestination = "authGraph"
+    ) {
         /* 인증/인가 */
-        composable(route = Route.Login.route) {
-            LoginScreen(
-                modifier = modifier,
-                onLoginClick = {
-                    // TODO: 로그인 로직 구현
-                    navController.navigate(Route.Home.route)
-                },
-                onRegisterClick = {
-                    navController.navigate(Route.SignUpID.route)
-                }
-            )
-        }
-        composable(route = Route.SignUpID.route) {
-            SignUpIDScreen(
-                modifier = modifier,
-                onNext = {
-                    navController.navigate(Route.SignUpPW.route)
-                }
-            )
-        }
-        composable(route = Route.SignUpPW.route) {
-            SignUpPWScreen(
-                modifier = modifier,
-                onNext = {
-                    navController.navigate(Route.SignUpName.route)
-                }
-            )
-        }
-        composable(route = Route.SignUpName.route) {
-            SignUpNameScreen(
-                modifier = modifier,
-                onNext = {
-                    navController.navigate(Route.SignUpPhone.route)
-                }
-            )
-        }
-        composable(route = Route.SignUpPhone.route) {
-            SignUpPhoneScreen(
-                modifier = modifier,
-                onNext = {
-                    navController.navigate(Route.SignUpVerification.route)
-                }
-            )
-        }
-        composable(route = Route.SignUpVerification.route) {
-            SignUpVerificationScreen(
-                modifier = modifier,
-                onComplete = {
-                    navController.navigate(Route.Login.route) {
-                        popUpTo(Route.Login.route) { inclusive = true }
-                    }
-                }
-            )
-        }
+        authNavGraph(navController, modifier)
 
         /*자산 조회(홈)*/
         composable(route = Route.Home.route) {
@@ -161,24 +100,7 @@ fun MoneyMateNavGraph(
                 }
             )
         }
-        retireNavGraph(navController,modifier)
-//        //은퇴시뮬레이션
-//        composable(route = Route.RetireInput.route) {
-//            RetireInputScreen(
-//                modifier = modifier,
-//                onNavigateBack = { navController.navigateUp() },
-//                onNavigateToRetireResult = {
-//                    navController.navigate(Route.RetireResult.route)
-//                }
-//            )
-//        }
-//        //은퇴시뮬레이션 결과
-//        composable(route = Route.RetireResult.route) {
-//            RetireResultScreen(
-//                modifier = modifier,
-//                onNavigateBack = { navController.navigateUp() }
-//            )
-//        }
+        retireNavGraph(navController, modifier)
 
         /* 마이페이지 */
         composable(route = Route.MyPage.route) {
