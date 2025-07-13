66c5899ce2cc39f2e771f3a98587368d38214671
casperjr <hyunjw0515@gmail.com>
2025-07-14 04:19:53 +0900

    [feat]: 로그인시 토큰들 TokenManager에 저장하고 로그로 값을 조회할 수 있도록 구현

--- app/src/main/java/com/moneymate/moneymate/data/repository/AuthRepository.kt
+++ app/src/main/java/com/moneymate/moneymate/data/repository/AuthRepository.kt
@@ -1,11 +1,15 @@
 package com.moneymate.moneymate.data.repository
 
+import android.util.Log
 import com.moneymate.moneymate.data.dto.auth.request.LoginRequest
 import com.moneymate.moneymate.data.dto.auth.request.RegisterRequest
 import com.moneymate.moneymate.data.service.AuthService
+import com.moneymate.moneymate.util.auth.TokenManager
+import kotlinx.coroutines.flow.first
 
 class AuthRepository(
-    private val authService: AuthService
+    private val authService: AuthService,
+    private val tokenManager: TokenManager
 ) {
     // 회원가입
     suspend fun registerUser(
@@ -31,11 +35,16 @@ class AuthRepository(
         userId: String,
         password: String
     ) = runCatching {
-        authService.login(
+        val response = authService.login(
             LoginRequest(
                 userId = userId,
                 password = password
             )
         )
+        // 로그인 성공 시 토큰 저장
+        tokenManager.saveAccessToken(response.accessToken)
+        tokenManager.saveRefreshToken(response.refreshToken)
+        Log.d("AuthRepository", "Access Token: ${tokenManager.getAccessToken().first()}")
+        Log.d("AuthRepository", "Refresh Token: ${tokenManager.getRefreshToken().first()}")
     }
 }
