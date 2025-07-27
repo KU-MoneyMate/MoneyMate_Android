3ed06ffa479e4f273c74d8df3b65390cd17a9e27
casperjr <hyunjw0515@gmail.com>
2025-07-28 06:29:34 +0900

    [feat]: 비밀번호 일치 여부에 따른 snackbar 구현

--- app/src/main/java/com/moneymate/moneymate/ui/auth/screen/SignUpPWScreen.kt
+++ app/src/main/java/com/moneymate/moneymate/ui/auth/screen/SignUpPWScreen.kt
@@ -2,6 +2,7 @@ package com.moneymate.moneymate.ui.auth.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,10 +13,14 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -33,6 +38,7 @@ import com.moneymate.moneymate.ui.common.BottomFullWidthButton
 import com.moneymate.moneymate.ui.common.MoneyMateTextField
 import com.moneymate.moneymate.ui.navigation.Route
 import com.moneymate.moneymate.ui.theme.MoneyMateTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignUpPWScreen(
@@ -42,88 +48,109 @@ fun SignUpPWScreen(
 ) {
     var pw by rememberSaveable { mutableStateOf("") }
     var confirmPw by rememberSaveable { mutableStateOf("") }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MoneyMateTheme.colors.white),
-        verticalArrangement = Arrangement.SpaceBetween
+    Box(
+        modifier = Modifier.fillMaxSize()
     ) {
         Column(
             modifier = Modifier
-                .padding(horizontal = 30.dp)
+                .fillMaxSize()
+                .background(color = MoneyMateTheme.colors.white),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Spacer(modifier = Modifier.height(75.dp))
-
-            Text(
-                text = "비밀번호를 입력해주세요",
-                color = MoneyMateTheme.colors.black,
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    fontFamily = FontFamily(Font(R.font.pretendard_bold))
-                )
-            )
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Text(
-                text = "영문, 숫자 포함 8자 이상",
-                color = MoneyMateTheme.colors.neutral500,
-                style = TextStyle(
-                    fontSize = 14.sp,
-                    fontFamily = FontFamily(Font(R.font.pretendard_regular))
-                )
-            )
-
-            Spacer(modifier = Modifier.height(12.dp))
-
-            MoneyMateTextField(
-                text = pw,
-                onValueChange = { pw = it },
-                modifier = Modifier.fillMaxWidth(),
-                placeholder = {
-                    Text(
-                        text = "비밀번호를 입력해주세요",
-                        style = MoneyMateTheme.typography.body_01_M_14
-                    )
-                }
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            MoneyMateTextField(
-                text = confirmPw,
-                onValueChange = { confirmPw = it },
-                modifier = Modifier.fillMaxWidth(),
-                placeholder = {
-                    Text(
-                        text = "확인을 위해 비밀번호를 다시 입력해주세요",
-                        style = MoneyMateTheme.typography.body_01_M_14
-                    )
-                }
-            )
-        }
-
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(30.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            BottomFullWidthButton(
-                containerColor = MoneyMateTheme.colors.deepBlue,
-                contentColor = Color.White,
-                text = "다음",
+            Column(
                 modifier = Modifier
-                    .width(320.dp)
-                    .padding(bottom = 30.dp)
+                    .padding(horizontal = 30.dp)
             ) {
-                if (pw == confirmPw) {
-                    viewModel.saveSignupPassword(
-                        password = pw
-                    ) {
-                        onNext()
+                Spacer(modifier = Modifier.height(75.dp))
+
+                Text(
+                    text = "비밀번호를 입력해주세요",
+                    color = MoneyMateTheme.colors.black,
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_bold))
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = "영문, 숫자 포함 8자 이상",
+                    color = MoneyMateTheme.colors.neutral500,
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontFamily = FontFamily(Font(R.font.pretendard_regular))
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                MoneyMateTextField(
+                    text = pw,
+                    onValueChange = { 
+                        pw = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = {
+                        Text(
+                            text = "비밀번호를 입력해주세요",
+                            style = MoneyMateTheme.typography.body_01_M_14
+                        )
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                MoneyMateTextField(
+                    text = confirmPw,
+                    onValueChange = { 
+                        confirmPw = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = {
+                        Text(
+                            text = "확인을 위해 비밀번호를 다시 입력해주세요",
+                            style = MoneyMateTheme.typography.body_01_M_14
+                        )
+                    }
+                )
+            }
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(30.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                BottomFullWidthButton(
+                    modifier = Modifier
+                        .width(320.dp)
+                        .padding(bottom = 30.dp),
+                    text = "다음",
+                    containerColor = MoneyMateTheme.colors.deepBlue,
+                    contentColor = MoneyMateTheme.colors.white,
+                ) {
+                    if (pw == confirmPw) {
+                        viewModel.saveSignupPassword(
+                            password = pw
+                        ) {
+                            onNext()
+                        }
+                    } else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("비밀번호가 일치하지 않습니다")
+                        }
                     }
                 }
             }
         }
+
+        SnackbarHost(
+            hostState = snackbarHostState,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 16.dp)
+        )
     }
 }
 
