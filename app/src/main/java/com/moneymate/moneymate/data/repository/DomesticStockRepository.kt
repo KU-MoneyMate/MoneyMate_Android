2218b2582dc3c0dac5de955f34b5ab370d60a153
casperjr <hyunjw0515@gmail.com>
2025-09-10 21:41:05 +0900

    [feat]: 추가된 baseurl에 대응되는 각각의 service, repository 생성

---
+++ app/src/main/java/com/moneymate/moneymate/data/repository/DomesticStockRepository.kt
@@ -0,0 +1,9 @@
+package com.moneymate.moneymate.data.repository
+
+import com.moneymate.moneymate.data.service.DomesticStockService
+
+class DomesticStockRepository(
+    private val domesticStockService: DomesticStockService
+) {
+
+}
---
+++ app/src/main/java/com/moneymate/moneymate/data/repository/ForeignStockRepository.kt
@@ -0,0 +1,8 @@
+package com.moneymate.moneymate.data.repository
+
+import com.moneymate.moneymate.data.service.ForeignStockService
+
+class ForeignStockRepository(
+    private val foreignStockService: ForeignStockService
+) {
+}
