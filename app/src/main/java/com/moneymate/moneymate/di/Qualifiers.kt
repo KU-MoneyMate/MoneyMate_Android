2e6afb130024c29efb2a26dc3d97e92a8752e4b4
casperjr <hyunjw0515@gmail.com>
2025-09-10 21:14:05 +0900

    [feat]: base url에 따른 retrofit 분리를 위한 qualifier 작성

---
+++ app/src/main/java/com/moneymate/moneymate/di/Qualifiers.kt
@@ -0,0 +1,23 @@
+package com.moneymate.moneymate.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ForeignStockRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class ForeignStockOkHttpClient
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DomesticStockRetrofit
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DomesticStockOkHttpClient
