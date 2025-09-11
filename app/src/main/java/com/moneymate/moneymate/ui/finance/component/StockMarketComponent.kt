36730b1e3b8da3889217383cd07b557fc49d230a
casperjr <hyunjw0515@gmail.com>
2025-09-12 00:05:03 +0900

    [feat]: 텍스트 길이가 긴 경우에 대한 처리

--- app/src/main/java/com/moneymate/moneymate/ui/finance/component/StockMarketComponent.kt
+++ app/src/main/java/com/moneymate/moneymate/ui/finance/component/StockMarketComponent.kt
@@ -1,6 +1,5 @@
 package com.moneymate.moneymate.ui.finance.component
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,15 +8,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.moneymate.moneymate.R
 import com.moneymate.moneymate.ui.theme.MoneyMateTheme
 
 @Composable
@@ -113,19 +110,22 @@ fun StockMarketItem(
     fluctuation: String,
     status: String
 ) {
-    Box(
+    Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(vertical = 4.dp, horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.align(Alignment.CenterStart),
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 16.dp),
             text = stockName,
-            style = MoneyMateTheme.typography.head_04_SB_14
+            style = MoneyMateTheme.typography.head_04_SB_14,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
         Column(
-            modifier = Modifier
-                .align(Alignment.CenterEnd),
             horizontalAlignment = Alignment.End
         ) {
             Text(
