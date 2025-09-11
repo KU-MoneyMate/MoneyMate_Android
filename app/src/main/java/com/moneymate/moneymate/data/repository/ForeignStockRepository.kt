0810753a02a68b433bbca6d1bea2e194adee2bd9
casperjr <hyunjw0515@gmail.com>
2025-09-11 21:24:48 +0900

    [feat]: 인덱스, 환율, 시장 정보를 위한 service, repository 작성

--- app/src/main/java/com/moneymate/moneymate/data/repository/ForeignStockRepository.kt
+++ app/src/main/java/com/moneymate/moneymate/data/repository/ForeignStockRepository.kt
@@ -5,4 +5,23 @@ import com.moneymate.moneymate.data.service.ForeignStockService
 class ForeignStockRepository(
     private val foreignStockService: ForeignStockService
 ) {
+    suspend fun getMarketIndexes() =
+        runCatching {
+            foreignStockService.getMarketIndexes()
+        }
+
+    suspend fun getForeignMarketCap(exchange: String, page: Int, pageSize: Int) =
+        runCatching {
+            foreignStockService.getForeignMarketCap(exchange, page, pageSize)
+        }
+
+    suspend fun getForeignMarketRising(exchange: String, page: Int, pageSize: Int) =
+        runCatching {
+            foreignStockService.getForeignMarketRising(exchange, page, pageSize)
+        }
+
+    suspend fun getForeignMarketFalling(exchange: String, page: Int, pageSize: Int) =
+        runCatching {
+            foreignStockService.getForeignMarketFalling(exchange, page, pageSize)
+        }
 }
--- app/src/main/java/com/moneymate/moneymate/data/service/ForeignStockService.kt
+++ app/src/main/java/com/moneymate/moneymate/data/service/ForeignStockService.kt
@@ -1,4 +1,37 @@
 package com.moneymate.moneymate.data.service
 
+import com.moneymate.moneymate.data.dto.finance.response.market.ForeignStockResponse
+import com.moneymate.moneymate.data.dto.finance.response.market.MarketIndexResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface ForeignStockService {
+    // 지수
+    @GET("index/major")
+    suspend fun getMarketIndexes(): List<MarketIndexResponse>
+
+    // 시가총액
+    @GET("stock/exchange/{exchange}/marketValue")
+    suspend fun getForeignMarketCap(
+        @Path("exchange") exchange: String,
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): ForeignStockResponse
+
+    // 상승
+    @GET("stock/exchange/{exchange}/up")
+    suspend fun getForeignMarketRising(
+        @Path("exchange") exchange: String,
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): ForeignStockResponse
+
+    // 하락
+    @GET("stock/exchange/{exchange}/down")
+    suspend fun getForeignMarketFalling(
+        @Path("exchange") exchange: String,
+        @Query("page") page: Int = 1,
+        @Query("pageSize") pageSize: Int = 20
+    ): ForeignStockResponse
 }
