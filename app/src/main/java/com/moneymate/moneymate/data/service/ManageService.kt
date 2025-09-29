626de2b20e6abd4c59c5ef553aedf6f45ee5cdaa
casperjr <hyunjw0515@gmail.com>
2025-09-29 18:01:36 +0900

    [feat]: 또래 통계 요청 service, repository 코드 작성

--- app/src/main/java/com/moneymate/moneymate/data/service/ManageService.kt
+++ app/src/main/java/com/moneymate/moneymate/data/service/ManageService.kt
@@ -3,6 +3,7 @@ package com.moneymate.moneymate.data.service
 import com.moneymate.moneymate.data.dto.manage.response.AssetStatHistoryResponse
 import com.moneymate.moneymate.data.dto.manage.request.RetireInputRequest
 import com.moneymate.moneymate.data.dto.manage.response.Asset
+import com.moneymate.moneymate.data.dto.manage.response.PeerStatResponse
 import com.moneymate.moneymate.data.dto.manage.response.SpendingStatsResponse
 import com.moneymate.moneymate.data.dto.manage.response.TotalAssetResponse
 import retrofit2.http.Body
@@ -33,4 +34,25 @@ interface ManageService {
         @Query("startDay") startDay : String,
         @Query("endDay") endDay : String
     ) : SpendingStatsResponse
+
+    // 또래 자산 통계 조회
+    @GET("stats/data/asset")
+    suspend fun getPeerAssetStat(
+        @Query("age") age: Int,
+        @Query("year") year: Int
+    ): PeerStatResponse
+
+    // 또래 소비 통계 조회
+    @GET("stats/data/consumption")
+    suspend fun getPeerConsumptionStat(
+        @Query("age") age: Int,
+        @Query("year") year: Int
+    ): PeerStatResponse
+
+    // 또래 소득 통계 조회
+    @GET("stats/data/income")
+    suspend fun getPeerIncomeStat(
+        @Query("age") age: Int,
+        @Query("year") year: Int
+    ): PeerStatResponse
 }
