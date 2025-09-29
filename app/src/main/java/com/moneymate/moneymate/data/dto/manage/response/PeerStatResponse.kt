044163d04d54d1f5d3f2f91eba73a1a1bd845193
casperjr <hyunjw0515@gmail.com>
2025-09-29 18:01:11 +0900

    [feat]: 또래 통계 response 작성

---
+++ app/src/main/java/com/moneymate/moneymate/data/dto/manage/response/PeerStatResponse.kt
@@ -0,0 +1,33 @@
+package com.moneymate.moneymate.data.dto.manage.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PeerStatResponse(
+    @SerialName("status")
+    val status: String,
+    @SerialName("message")
+    val message: String,
+    @SerialName("data")
+    val data: List<Data>
+) {
+    @Serializable
+    data class Data(
+        @SerialName("age")
+        val age: Int,
+        @SerialName("c2")
+        val c2: String,
+        @SerialName("item")
+        val item: String,
+        @SerialName("itemName")
+        val itemName: String,
+        @SerialName("year")
+        val year: String,
+        @SerialName("unitName")
+        val unitName: String,
+        @SerialName("value")
+        val value: Double
+    )
+}
