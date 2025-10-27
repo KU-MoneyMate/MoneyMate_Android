5b327d124e7ff2a6b52d5e46bd378707aaa5e296
casperjr <hyunjw0515@gmail.com>
2025-10-27 22:28:38 +0900

    [CHORE]: 주식 이미지 요청 코드 추가

--- app/src/main/java/com/moneymate/moneymate/data/service/StockIconService.kt
+++ app/src/main/java/com/moneymate/moneymate/data/service/StockIconService.kt
@@ -1,8 +1,12 @@
 package com.moneymate.moneymate.data.service
 
+import okhttp3.ResponseBody
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface StockIconService {
-    @GET("stock_icons")
-    suspend fun getStockIcon()
+    @GET("stock_icons/{ticker}.svg")
+    suspend fun getStockIcon(
+        @Path("ticker") ticker: String
+    ): ResponseBody
 }
