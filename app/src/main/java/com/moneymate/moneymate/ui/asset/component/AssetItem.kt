e463c7aeed4047e26c6b642d0540ee805aa577d0
casperjr <hyunjw0515@gmail.com>
2025-10-27 14:21:40 +0900

    [REFACTOR]: 자산 아이콘 placeholder 대체

--- app/src/main/java/com/moneymate/moneymate/ui/asset/component/AssetItem.kt
+++ app/src/main/java/com/moneymate/moneymate/ui/asset/component/AssetItem.kt
@@ -1,5 +1,6 @@
 package com.moneymate.moneymate.ui.asset.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,11 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,11 +36,11 @@ fun AssetItem(
             modifier = Modifier,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
+            Image(
                 modifier = Modifier.size(45.dp),
-                painter = painterResource(R.drawable.img_dummy_asset),
+                painter = painterResource(R.drawable.ic_moneymatelogo),
                 contentDescription = "asset icon",
-                tint = MoneyMateTheme.colors.lightGray
+                contentScale = ContentScale.Fit
             )
             Spacer(modifier = Modifier.size(17.dp))
             Column(
